324442bcea470398bb6066d2b05fb8226cde3671
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-17 18:03:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let i = 0;
 
 function init() {
-  document.body.addEventListener(e => alert(e.which))
+  document.body.addEventListener("keydown", e => alert(e.which))
 }
 
 init();
