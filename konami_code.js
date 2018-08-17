006e150e3cd69581795ce18982881f0266ed94e2
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-17 18:06:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,13 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let i = 0;
 
 function init() {
-  document.body.addEventListener("keydown", e => alert(e.which))
+  document.body.addEventListener("keydown", e => {
+    if (e.which == code[i]){
+      i++
+    }
+    else i=0;
+    if (i==code.length) alert("KONAMI code activated");
+  })
 }
 
 init();
